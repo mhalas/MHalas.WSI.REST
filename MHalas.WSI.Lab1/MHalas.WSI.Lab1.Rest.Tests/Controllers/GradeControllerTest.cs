0f7de81ac3072b6df9145ffc77d148481975961b
Maciej Hałas <maciejhalas@outlook.com>
2018-04-07 00:19:52 +0200

    Add mini change

--- MHalas.WSI.Lab1/MHalas.WSI.Lab1.Rest.Tests/Controllers/GradeControllerTest.cs
+++ MHalas.WSI.Lab1/MHalas.WSI.Lab1.Rest.Tests/Controllers/GradeControllerTest.cs
@@ -4,6 +4,11 @@ using MHalas.WSI.Lab1.Rest.Controllers;
 
 namespace MHalas.WSI.Lab1.Rest.Tests.Controllers
 {
+    [TestClass]
+    public class GradeControllerTest
+    {
+
+    }
     //[TestMethod]
     //public void Get_AllGrades_Success()
     //{
